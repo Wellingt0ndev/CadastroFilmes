5dab26ea7750376e6b9b07c79d7b07433e091911
Wellington Ferreira da Silva <108769429+Wellingt0ndev@users.noreply.github.com>
2024-02-27 18:06:09 -0300

    Created FilmeController

---
+++ CadastroFilmes.Web/Models/Filme.cs
@@ -0,0 +1,28 @@
+﻿namespace CadastroFilmes.Web.Models
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Genero { get; set; }
+
+        public Filme()
+        {
+        }
+
+        public Filme(string nome)
+        {
+            Nome = nome;
+        }
+
+        public Filme(string nome, string genero) : this(nome)
+        {
+            Genero = genero;            
+        }
+
+        public Filme(int id, string nome, string genero):this(nome,genero)
+        {
+            Id = id;            
+        }
+    }
+}
